e44c1e276987e17d61c285abd3ff373a6f843235
蒼時弦也 <elct9620@frost.tw>
2012-12-19 11:32:48 +0800

    Update# Make menu work

--- coffeescripts/main.coffee
+++ coffeescripts/main.coffee
@@ -1,1 +1,23 @@
-# Hello
+
+$(document).ready ->
+
+  ###
+  # Menu Manager
+  ###
+
+  $("#main_menu a").click (e)->
+
+    target = $($(this).attr("href"))
+
+    offset = 0
+
+    if target.hasClass("attach_arrow_green") || target.hasClass("attach_arrow_black") || target.hasClass("attach_arrow_white")
+      offset = 35
+
+    $("html, body").animate {
+      scrollTop: target.offset().top - 298 + offset;
+    }, {druation: 3000, easing: "easeInOutQuad"}
+
+    e.preventDefault();
+
+
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,24 @@
 (function() {
 
-
+  $(document).ready(function() {
+    /*
+      # Menu Manager
+    */
+    return $("#main_menu a").click(function(e) {
+      var offset, target;
+      target = $($(this).attr("href"));
+      offset = 0;
+      if (target.hasClass("attach_arrow_green") || target.hasClass("attach_arrow_black") || target.hasClass("attach_arrow_white")) {
+        offset = 35;
+      }
+      $("html, body").animate({
+        scrollTop: target.offset().top - 298 + offset
+      }, {
+        druation: 3000,
+        easing: "easeInOutQuad"
+      });
+      return e.preventDefault();
+    });
+  });
 
 }).call(this);
