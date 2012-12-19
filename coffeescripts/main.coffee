fec0a98898d883a43ea62b4e2622f08ef021b653
蒼時弦也 <elct9620@frost.tw>
2012-12-19 15:21:44 +0800

    Update# Optimize menu, update content, using google maps api

--- coffeescripts/main.coffee
+++ coffeescripts/main.coffee
@@ -5,6 +5,10 @@ $(document).ready ->
   # Menu Manager
   ###
 
+  $("html, body").animate {
+    scrollTop: 0
+  }, {druation: 3000, easing: "easeInOutQuad"}
+
   $("#main_menu a").click (e)->
 
     target = $($(this).attr("href"))
@@ -15,9 +19,50 @@ $(document).ready ->
       offset = 35
 
     $("html, body").animate {
-      scrollTop: target.offset().top - 298 + offset;
+      scrollTop: target.offset().top - 248 + offset
     }, {druation: 3000, easing: "easeInOutQuad"}
 
     e.preventDefault();
 
+  ###
+  # Google Maps
+  ###
+
+  mapCenter = new google.maps.LatLng(25.014218,121.54065)
+
+  mapCanvas = $("#map_canvas")
+
+  mapOptions= {
+    center: mapCenter,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    mapTypeControl: false,
+    scrollwheel: false,
+    zoomControlOptions: {
+      style: google.maps.ZoomControlStyle.SMALL
+    },
+    zoom: 16
+  }
+
+  mapCanvas.width('100%')
+  mapCanvas.height(400)
+
+  map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions)
+  mapMarker = new google.maps.Marker({position: mapCenter, map: map, icon: '../images/location_pin.png'})
+
+###
+# Scroll Menu
+###
+
+$(window).scroll (e)->
+  curTop = $("body").scrollTop()
+
+  $("#main_menu a").each (i, el)->
+    target = $($(el).attr("href"))
+    offsetTop = Math.floor(target.offset().top) - 250
+    height = $(target).outerHeight()
+
+    if (curTop >= offsetTop) and (curTop < (offsetTop + height))
+      $(el).addClass("active")
+    else
+      $(el).removeClass("active")
 
--- javascripts/main.js
+++ javascripts/main.js
@@ -4,7 +4,15 @@
     /*
       # Menu Manager
     */
-    return $("#main_menu a").click(function(e) {
+
+    var map, mapCanvas, mapCenter, mapMarker, mapOptions;
+    $("html, body").animate({
+      scrollTop: 0
+    }, {
+      druation: 3000,
+      easing: "easeInOutQuad"
+    });
+    $("#main_menu a").click(function(e) {
       var offset, target;
       target = $($(this).attr("href"));
       offset = 0;
@@ -12,12 +20,57 @@
         offset = 35;
       }
       $("html, body").animate({
-        scrollTop: target.offset().top - 298 + offset
+        scrollTop: target.offset().top - 248 + offset
       }, {
         druation: 3000,
         easing: "easeInOutQuad"
       });
       return e.preventDefault();
+    });
+    /*
+      # Google Maps
+    */
+
+    mapCenter = new google.maps.LatLng(25.014218, 121.54065);
+    mapCanvas = $("#map_canvas");
+    mapOptions = {
+      center: mapCenter,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      mapTypeControl: false,
+      scrollwheel: false,
+      zoomControlOptions: {
+        style: google.maps.ZoomControlStyle.SMALL
+      },
+      zoom: 16
+    };
+    mapCanvas.width('100%');
+    mapCanvas.height(400);
+    map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+    return mapMarker = new google.maps.Marker({
+      position: mapCenter,
+      map: map,
+      icon: '../images/location_pin.png'
+    });
+  });
+
+  /*
+  # Scroll Menu
+  */
+
+
+  $(window).scroll(function(e) {
+    var curTop;
+    curTop = $("body").scrollTop();
+    return $("#main_menu a").each(function(i, el) {
+      var height, offsetTop, target;
+      target = $($(el).attr("href"));
+      offsetTop = Math.floor(target.offset().top) - 250;
+      height = $(target).outerHeight();
+      if ((curTop >= offsetTop) && (curTop < (offsetTop + height))) {
+        return $(el).addClass("active");
+      } else {
+        return $(el).removeClass("active");
+      }
     });
   });
 
