2916b8077c21318e48f347c900e4f0e127513f08
蒼時弦也 <elct9620@frost.tw>
2013-02-08 15:15:54 +0800

    Update# Update sponsor list

--- config.rb
+++ config.rb
@@ -23,4 +23,6 @@ relative_assets = true
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
 
 sass_options = {:debug_info=>true} # by Fire.app 
-output_style = :expanded # by Fire.app 
+line_comments = true # by Fire.app 
+
+output_style = :compressed # by Fire.app 
